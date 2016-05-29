4738acbd0883ac5b8f4c1cc9bd8dc29d89cd083a
Roman Shevchuk <rommssh@gmail.com>
2016-05-29 16:04:18 +0300

    Bug with pagination fixed

--- ClassicUI/ShowAll/ShowAll.js
+++ ClassicUI/ShowAll/ShowAll.js
@@ -12,8 +12,9 @@ function makeMagick(){
         links.each(function(){
             $this = $(this);
             var url = $this.attr('href');
-            if(url.match(/(rowsperpage)=[0-9]*/g)){
-                url = url.replace(/(rowsperpage)=[0-9]*/g, "$1=1000000");
+            if(url.match(/(%3Arowsperpage)=[0-9]*/g)){
+                url = url.replace(/(%3Arowsperpage)=[0-9]*/g, "$1=1000000");
+				url = url.replace(/(%3Alsr)=[0-9]*/g, "$1=0");
                 
                 $this.parent().append('&nbsp;<a href="'+ url +'">SHOW ALL</a>');
                 console.log(url);
@@ -29,9 +30,16 @@ function makeMagick(){
 			$link = $($link.get(0));
 			var url = $link.attr('href');
 
-            if(url.match(/(rowsperpage)=[0-9]*/g) && url.match(/(Alsr)=[0-9]*/g)){
-                url = url.replace(/(rowsperpage)=[0-9]*/g, "$1=1000000");
-                url = url.replace(/(Alsr)=[0-9]*/g, "$1=0");
+            if(url.match(/(%3Alsr)=[0-9]*/g)){
+                url = url.replace(/(%3Alsr)=[0-9]*/g, "$1=0");
+				
+                console.log('second' + url);
+
+				if(url.match(/(%3Arowsperpage)=[0-9]*/g)){
+					url = url.replace(/(%3Arowsperpage)=[0-9]*/g, "$1=1000000");
+				} else {
+					url = url.replace(/([&?]([^&]*%3A)(lsr)=[0-9]*)/g, "$1&$2rowsperpage=1000000");
+				}
                 
                 $link.parent().append('&nbsp;|&nbsp;<a href="'+ url +'">SHOW ALL</a>');
                 console.log('second' + url);
